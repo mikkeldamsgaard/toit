766a9c7dc5d7cc84e5f256ff8b302cce1dd4f173
mikkel <mikkel@spottune.com>
2022-03-23 09:44:20 +0100

    Fixed funky CLion indentation changes, while copy paste

--- tests/ctest/external_messaging_test.cc
+++ tests/ctest/external_messaging_test.cc
@@ -23,20 +23,20 @@ static SnapshotBundle compile(const char* input_path) {
                           null,
                           args,
                           out_path, {
-                              .dep_file = null,
-                              .dep_format = compiler::Compiler::DepFormat::none,
-                              .project_root = null,
-                              .force = false,
-                              .werror = true,
+                            .dep_file = null,
+                            .dep_format = compiler::Compiler::DepFormat::none,
+                            .project_root = null,
+                            .force = false,
+                            .werror = true,
                           });
 }
 
 class MessageHandler : public ExternalSystemMessageHandler {
-public:
+ public:
   explicit MessageHandler(VM* vm) : ExternalSystemMessageHandler(vm) { }
   virtual void on_message(int sender, int type, void* data, int length) override;
 
-private:
+ private:
   bool _try_hard = false;
 };
 
