73bd82df1316c97dcf194152f15ad8985c932bed
mikkel <mikkel@spottune.com>
2022-07-13 11:12:26 +0200

    MVP - Extender - waspot test app fixes

--- src/visitor.cc
+++ src/visitor.cc
@@ -55,6 +55,7 @@ void Visitor::accept(Object* object) {
       visit_task(Task::cast(heap_object));
       break;
   default:
+    printf("%d\n", heap_object->class_tag());
     FATAL("Unexpected class tag");
   }
 }
